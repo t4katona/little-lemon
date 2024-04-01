517c001857553746c4fe7fd767bf19a3ecacef62
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-04-01 10:10:26 +0200

    Created the controlled form

--- src/components/form/BookingForm.jsx
+++ src/components/form/BookingForm.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./bookingForm.module";
 
 export const BookingForm = () => {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
   return (
     <form className="general_padding">
       <label htmlFor="res-date">Choose date</label>
-      <input type="date" name="res-date" id="res-date" />
+      <input
+        type="date"
+        name="res-date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
       <label htmlFor="res-time">Choose time</label>
-      <select name="res-time" id="res-time">
+      <select
+        name="res-time"
+        id="res-time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      >
         <option>17:00</option>
         <option>18:00</option>
         <option>19:00</option>
@@ -16,9 +31,22 @@ export const BookingForm = () => {
         <option>22:00</option>
       </select>
       <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={guests}
+        onChange={(e) => setGuests(e.target.value)}
+      />
       <label htmlFor="occasion">Occasion</label>
-      <select name="occasion" id="occasion">
+      <select
+        name="occasion"
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+      >
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
