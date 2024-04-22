1f3735a2bd76fdd53a611a8baed0c947aea37e61
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-04-22 22:50:01 +0200

    added a unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
-import BookingForm from "./components/form/BookingForm";
+import { BookingForm } from "./components/form/BookingForm";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the BookingForm heading", () => {
+  const availableTimesMock = ["10:00", "11:00", "12:00", "13:00"];
+  const submitFormMock = {
+    date: "2024-04-22",
+    time: "10:00",
+    guests: "1",
+    occasion: "Birthday",
+  };
+  render(
+    <BookingForm
+      availableTimes={availableTimesMock}
+      updateTimes="2024-04-22"
+      submitForm={submitFormMock}
+    />
+  );
+  const headingElement = screen.getByText("Book Now");
+  expect(headingElement).toBeInTheDocument();
 });
